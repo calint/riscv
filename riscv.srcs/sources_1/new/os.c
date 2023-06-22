ce3fe7ee5eb6d3308972b48a3d47c797e1ce6abf
calint <calin.tenitchi@gmail.com>
2023-06-22 16:48:20 +0200

    .

--- riscv.srcs/sources_1/new/os.c
+++ riscv.srcs/sources_1/new/os.c
@@ -1,6 +1,7 @@
 #define CPU_FREQ_HZ 50000000
 #define TICKS_PER_SEC CPU_FREQ_HZ / 60
 #define TOP_OF_RAM 0x1ffff
+#define TOP_OF_STACK 0x1fffc
 
 volatile unsigned char *leds = (unsigned char *)TOP_OF_RAM;
 volatile unsigned char *uart_out = (unsigned char *)TOP_OF_RAM - 1;
@@ -22,7 +23,7 @@ static char *hello = "Hello World\r\n";
 
 void _start() {
   // stack to top of memory minus the mapped leds
-  set_stack_pointer((void *)(TOP_OF_RAM - 2));
+  set_stack_pointer((void *)(TOP_OF_STACK));
 
   char *p = hello;
   while (*p) {
