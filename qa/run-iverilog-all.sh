ca110ac1787c28781eb24608c823e11e9a287606
calint <calin.tenitchi@gmail.com>
2024-12-20 19:28:31 +0700

    .

--- qa/run-iverilog-all.sh
+++ qa/run-iverilog-all.sh
@@ -2,12 +2,9 @@
 #
 # note: run from project root. i.e. qa/run-iverilog-all.sh
 #
+set -e
 
-echo sim_4
-qa/run-iverilog.sh sim_4 TB_SoC 2>&1 | grep FAILED
-echo sim_3
-qa/run-iverilog.sh sim_3 TB_SoC 2>&1 | grep FAILED
-echo sim_2
-qa/run-iverilog.sh sim_2 TB_RAM_Interface 2>&1 | grep FAILED
-echo sim_1
-qa/run-iverilog.sh sim_1 TB_SoC 2>&1 | grep FAILED
+qa/run-iverilog.sh sim_4 TB_SoC
+qa/run-iverilog.sh sim_3 TB_SoC
+qa/run-iverilog.sh sim_2 TB_RAM_Interface
+qa/run-iverilog.sh sim_1 TB_SoC
